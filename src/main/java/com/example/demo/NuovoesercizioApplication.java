8e3d61151266f7e4f630f6707882a34e2bcb215b
Francesca Arata <francescaarata@live.it>
2023-07-24 16:48:26 +0200

    esercizio1

---
+++ src/main/java/com/example/demo/NuovoesercizioApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class NuovoesercizioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuovoesercizioApplication.class, args);
+		log.info("Ciao Mondo");
+	}
+
+}
